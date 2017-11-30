31ab33850f1ee661bed5aefa92caed4b47dcd32f
root <root@ar-web-01.c.optimum-web-161509.internal>
2017-11-30 11:38:00 +0000

    Doc. Better example and fix file not found

--- README.md
+++ README.md
@@ -1,8 +1,55 @@
 GeoIP lookup
 =============
+Generic PHP-codebase to easily switch between MaxMind/IP2Location.
+
 Lightweight codebase for MaxMind and IP2Location.
 - No composer/autoload for MaxMind
-- IP2Location without MySQL (binfile)
+- IP2Location without autoload/mysql (binfile)
+
+Example in
+```
+<?php
+require "path/to/this/repo/index.php";
+echo "<pre>";
+$res = ip2loc($_SERVER["HTTP_X_FORWARDED_FOR"]);
+var_dump($res);
+
+$res = maxmind($_SERVER["HTTP_X_FORWARDED_FOR"]);
+var_dump($res);
+```
+
+Example out
+```
+array(6) {
+  ["ip"]=>
+  string(14) "94.232.119.106"
+  ["ipv"]=>
+  int(4)
+  ["iso2"]=>
+  string(2) "SM"
+  ["city"]=>
+  string(10) "San Marino"
+  ["lat"]=>
+  float(43.933330535889)
+  ["long"]=>
+  float(12.449999809265)
+}
+array(6) {
+  ["ip"]=>
+  string(14) "94.232.119.106"
+  ["ipv"]=>
+  int(4)
+  ["iso2"]=>
+  string(2) "SM"
+  ["city"]=>
+  NULL
+  ["lat"]=>
+  float(43.9333)
+  ["long"]=>
+  float(12.4667)
+}
+```
+
 
 MaxMind
 ```
--- index.php
+++ index.php
@@ -1,2 +1,35 @@
 <?php
-require __DIR__ . "/ip2location.php";
+function ip2loc($ip) {
+    require_once __DIR__ . '/vendor/ip2location/ip2location-php/IP2Location.php';
+    $f = "/usr/local/share/GeoIP/IP2LOCATION-LITE-DB11.BIN";
+    if (! filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+        // IPv6
+        $f = "/usr/local/share/GeoIP/IP2LOCATION-LITE-DB11.IPV6.BIN";
+    }
+    $db = new \IP2Location\Database($f, \IP2Location\Database::FILE_IO);
+    $records = $db->lookup($ip, \IP2Location\Database::ALL);
+
+    return [
+        "ip" => $records['ipAddress'],
+        "ipv" => $records['ipVersion'],
+        "iso2" => $records['countryCode'],
+        "city" => $records['cityName'],
+        "lat" => $records['latitude'],
+        "long" => $records['longitude']
+    ];
+}
+
+function maxmind($ip) {
+    require_once __DIR__ . '/geoip2.phar';
+    $reader = new \GeoIp2\Database\Reader('/usr/local/share/GeoIP/GeoLite2-City.mmdb');
+    $record = $reader->city($ip);
+
+    return [
+        "ip" => $ip,
+        "ipv" => filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) ? 4 : 6,
+        "iso2" => $record->country->isoCode,
+        "city" => $record->city->name,
+        "lat" => $record->location->latitude,
+        "long" => $record->location->longitude
+    ];
+}
