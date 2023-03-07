13e5bc8197a45e74ae3ef44ed581cffe43d96557
root <root@xsnews-com-nyc1-01>
2023-03-07 16:16:32 +0100

    Bugfix. Switch wget to curl for verbose failures and ensure all dirs are there

--- update.sh
+++ update.sh
@@ -5,19 +5,22 @@ IFS=$'\n\t'
 
 rm -rf "/tmp/geoip"
 mkdir -p "/tmp/geoip"
+mkdir -p "/usr/local/share/GeoIP"
 
 # IP2Loc
-wget -q "http://www.ip2location.com/download/?token=0TZdN4zFJFYvV1crcKnodfdRZPG5C5eDZdRMiqr16qx1rKtEuQ2H6gy65fcIkqmk&file=DB11LITEBIN" -O /tmp/geoip/DB11LITEBIN.zip
+curl --fail --silent --show-error "https://www.ip2location.com/download/?token=0TZdN4zFJFYvV1crcKnodfdRZPG5C5eDZdRMiqr16qx1rKtEuQ2H6gy65fcIkqmk&file=DB11LITEBIN" -o /tmp/geoip/DB11LITEBIN.zip
 cd /tmp/geoip
 unzip -o -q DB11LITEBIN.zip
+mv IP2LOCATION-LITE-DB11.BIN "/usr/local/share/GeoIP"
 
-wget -q "http://www.ip2location.com/download/?token=0TZdN4zFJFYvV1crcKnodfdRZPG5C5eDZdRMiqr16qx1rKtEuQ2H6gy65fcIkqmk&file=DB11LITEBINIPV6" -O /tmp/geoip/DB11LITEBINIPV6.zip
+curl --fail --silent --show-error "https://www.ip2location.com/download/?token=0TZdN4zFJFYvV1crcKnodfdRZPG5C5eDZdRMiqr16qx1rKtEuQ2H6gy65fcIkqmk&file=DB11LITEBINIPV6" -o /tmp/geoip/DB11LITEBINIPV6.zip
 cd /tmp/geoip
 unzip -o -q DB11LITEBINIPV6.zip
+mv IP2LOCATION-LITE-DB11.IPV6.BIN "/usr/local/share/GeoIP"
 
 # Maxmind
-wget -q "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=HIDDEN&suffix=tar.gz" -O /tmp/geoip/city.mmdb.tgz
-wget -q "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=HIDDEN&suffix=tar.gz" -O /tmp/geoip/asn.mmdb.tgz
+wget -nv "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=HIDDEN&suffix=tar.gz" -O /tmp/geoip/city.mmdb.tgz
+wget -nv "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=HIDDEN&suffix=tar.gz" -O /tmp/geoip/asn.mmdb.tgz
 
 tar -zxf /tmp/geoip/city.mmdb.tgz -C /tmp/geoip --strip-components=1
 tar -zxf /tmp/geoip/asn.mmdb.tgz -C /tmp/geoip --strip-components=1
@@ -25,4 +28,4 @@ tar -zxf /tmp/geoip/asn.mmdb.tgz -C /tmp/geoip --strip-components=1
 mv /tmp/geoip/GeoLite2-City.mmdb /usr/local/share/GeoIP/city.mmdb
 mv /tmp/geoip/GeoLite2-ASN.mmdb /usr/local/share/GeoIP/asn.mmdb
 
-cp -R * "/usr/local/share/GeoIP" && rm -rf "/tmp/geoip"
+rm -rf "/tmp/geoip"
